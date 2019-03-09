91ebe0b02500fb7619a082e97318b64869c32d24
Boban.S <boban.sugareski@gsix.me>
2019-03-09 16:37:11 +0100

    Adding MongoDB connection, product model and all operations over product

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,40 +1,84 @@
 const express = require('express')
 const router = express.Router()
+const Product = require('../models/product')
+const mongoose = require('mongoose')
 
 router.get('/', (request, response, next) => {
-    response.status(200).json({
-        message: 'Handling a GET request to /products'
-    })
+    Product.find()
+        .exec()
+        .then(products => {
+            response.status(200).json({
+                message: 'Handling a GET request to /products',
+                products: products
+            })
+        })
+        .catch(error => {
+            response.status(500).json({
+                error: error
+            })
+        })
 })
 
 router.post('/', (request, response, next) => {
-    const product = {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId,
         name: request.body.name,
         price: request.body.price
-    }
-
-    response.status(201).json({
-        message: 'Handling a POST request to /products',
-        product: product
     })
+
+    product.save().then(result => {
+        response.status(201).json({
+            message: 'Handling a POST request to /products',
+            product: product
+        })
+    }).catch(error => {
+        response.status(500).json({
+            message: 'Handling a POST request to /products',
+            error: error
+        })
+    }) 
 })
 
 router.get('/:id', (request, response, next) => {
-    response.status(200).json({
-        message: 'Handling a GET request to /products/:id'
-    })
+    Product.findById(request.params.id)
+        .exec()
+        .then(doc => {
+            doc ? response.status(201).json(doc) : response.status(404).json({
+                message: 'No product id matches the provided id'
+            })
+        })
+        .catch(error => {
+            response.status(500).json({error: error})
+        })
 })
 
 router.patch('/:id', (request, response, next) => {
-    response.status(200).json({
-        message: 'Handling a PATCH request to /products/:id'
-    })
+    const updateOps = {};
+    
+    for(const ops of request.body) {
+        updateOps[ops.propName] = ops.value
+    }
+
+    Product.update({ _id: request.params.id}, { $set: updateOps })
+        .exec()
+        .then(result => {
+            response.status(200).json(result)
+        })
+        .catch(error => {
+            console.log(error)
+            response.status(500).json({ error: error })
+        })
 })
 
 router.delete('/:id', (request, response, next) => {
-    response.status(200).json({
-        message: 'Handling a DELETE request to /products/:id'
-    })
+    Product.remove({_id: request.params.id})
+        .exec()
+        .then(result => {
+            response.status(200).json(result)
+        })
+        .catch(error => {
+            response.status(500).json({error: error})
+        })
 }) 
 
 module.exports = router
--- app.js
+++ app.js
@@ -4,42 +4,55 @@ const productRoutes = require('./api/routes/products')
 const ordersRoutes = require('./api/routes/orders')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+// DB connection
+mongoose.connect(
+    'mongodb+srv://' + 
+    process.env.MONGO_ATLAS_USERNAME + `:` + 
+    process.env.MONGO_ATLAS_PASSWORD + 
+    '@restful-shopping-ngbhd.mongodb.net/test?retryWrites=true',
+    { useNewUrlParser: true }
+)
 
 app.use(morgan('dev'))
 
+// request body parser
 app.use(bodyParser.urlencoded({
     extended: false
-}))
-app.use(bodyParser.json())
+})).use(bodyParser.json())
 
+// CORS handler
 app.use((request, response, next) => {
     response.header('Access-Control-Allow-Origin', '*')
-            .header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+        .header('Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization')
 
     if (request.method === 'OPTIONS') {
         response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
 
         return response.status(200).json({})
     }
+
+    next()
 })
 
 app.use('/products', productRoutes)
 app.use('/orders', ordersRoutes)
 
+// error handling
 app.use((request, response, next) => {
     const error = new Error('Not found')
 
     error.status = 404
     next(error)
-})
-
-app.use((error, request, response, next) => {
-    response.status(error.status || 500)
+    }).use((error, request, response, next) => {
+        response.status(error.status || 500)
             .json({
             error: {
                 message: error.message
             }
+        })
     })
-})
 
 module.exports = app

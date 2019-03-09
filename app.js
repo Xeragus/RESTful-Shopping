919619a691cf0f951e2c6be3daeca953fccc0535
Boban.S <boban.sugareski@gsix.me>
2019-03-09 13:34:48 +0100

    Adding basic error handling

--- app.js
+++ app.js
@@ -2,8 +2,27 @@ const express = require('express')
 const app = express()
 const productRoutes = require('./api/routes/products')
 const ordersRoutes = require('./api/routes/orders')
+const morgan = require('morgan')
+
+app.use(morgan('dev'))
 
 app.use('/products', productRoutes)
 app.use('/orders', ordersRoutes)
+
+app.use((request, response, next) => {
+    const error = new Error('Not found')
+
+    error.status = 404
+    next(error)
+})
+
+app.use((error, request, response, next) => {
+    response.status(error.status || 500)
+            .json({
+            error: {
+                message: error.message
+            }
+    })
+})
 
 module.exports = app
